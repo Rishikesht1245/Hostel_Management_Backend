55722e0de031769fd5776b8f82cdbb626372c573
Rishikesh T <rishikeshtgit@gmail.com>
2023-11-01 19:34:35 +0530

    Complete block controller

--- src/controllers/chiefWarden/block.ts
+++ src/controllers/chiefWarden/block.ts
@@ -20,3 +20,43 @@ export const allBlocks: RequestHandler = asyncHandler(async (req, res) => {
   const blocksData = await service.allBlocks();
   res.status(200).json(dataFormatter(blocksData));
 });
+
+// delete block
+export const deleteBlock: RequestHandler = asyncHandler(async (req, res) => {
+  await service.deleteBlock(req.params._id);
+  res.status(200).json(dataFormatter("Block deleted successfully"));
+});
+
+//check room availability
+export const checkRoomAvailability: RequestHandler = asyncHandler(
+  async (req, res) => {
+    const roomStatus = await service.getRoomAvailability(req.params.roomCode);
+    res.status(200).json(dataFormatter(roomStatus));
+  }
+);
+
+//  update room
+export const updateRoom = asyncHandler(async (req, res) => {
+  // const {status, student}  requires student data
+  res.json(dataFormatter(req.body));
+});
+
+//  available rooms in a block
+export const availableRooms: RequestHandler = asyncHandler(async (req, res) => {
+  const availableRooms = await service.availableRooms(req.params._id);
+  res.status(200).json(dataFormatter(availableRooms));
+});
+
+// block data with filtering
+export const blockData: RequestHandler = asyncHandler(async (req, res) => {
+  const blockData = await service.blockDetailsByName(req.params.name[0]);
+  res.status(200).json(dataFormatter(blockData));
+});
+
+//change room availability
+export const changeRoomAvailability: RequestHandler = asyncHandler(
+  async (req, res) => {
+    await service.changeRoomAvailability(req.params.code);
+    res.status(200).json(dataFormatter(`${req.params.code} updated`));
+  }
+);
