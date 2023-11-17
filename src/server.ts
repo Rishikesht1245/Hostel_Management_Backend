6b417ed6dfe69b60d64036e7bceb55610e6ebbea
Rishikesh T <rishikeshtgit@gmail.com>
2023-11-18 03:07:44 +0530

    attached server to socket

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 import app from "./app";
+import { io } from "./config/socket";
+import { socketAPI } from "./utils/socketApi";
 
 const PORT = process.env.PORT;
 
 const server = app.listen(PORT, () => {
   console.log(`server is running on http://localhost:${PORT}`);
 });
+
+// socket integration
+io.attach(server);
+socketAPI();
